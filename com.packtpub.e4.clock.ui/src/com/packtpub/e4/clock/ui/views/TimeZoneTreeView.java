9492d47205273c6e499988e8600c3ed24cc24b5c
BorodinovI <igordin@yandex.ru>
2014-04-02 17:00:01 +0400

    TreView : Interaction and properties

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTreeView.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/views/TimeZoneTreeView.java
@@ -1,7 +1,9 @@
 package com.packtpub.e4.clock.ui.views;
 
 import java.net.URL;
+import java.util.TimeZone;
 
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.FontRegistry;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
@@ -10,10 +12,16 @@ import org.eclipse.jface.resource.LocalResourceManager;
 import org.eclipse.jface.resource.ResourceManager;
 import org.eclipse.jface.viewers.ColumnViewer;
 import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
+import org.eclipse.jface.viewers.DoubleClickEvent;
+import org.eclipse.jface.viewers.IDoubleClickListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.part.ViewPart;
 
 import com.packtpub.e4.clock.ui.internal.TimeZoneComparator;
@@ -43,15 +51,45 @@ public class TimeZoneTreeView extends ViewPart {
 						new TimeZoneLabelProvider(ir,fr)));
 		treeViewer.setContentProvider(new TimeZoneContentProvider());
 		treeViewer.setInput(new Object[]{TimeZoneComparator.getTimeZones()});
+		
+		// Sorting and filtering
 		treeViewer.setData("REVERSE", Boolean.FALSE);
 		treeViewer.setComparator(new TimeZoneViewerComparator());
 		treeViewer.addFilter(new TimeZoneViewerFilter("GMT"));
 		//To remove the expandable nodes next to the tree items, the TreeViewercan be 
 		//configured to expand nodes automatically:
 		//treeViewer.setExpandPreCheckFilters(true);
-		
 		//treeViewer.expandAll();
 		treeViewer.expandToLevel(2);
+
+		// Interaction and properties
+		treeViewer.addDoubleClickListener(new IDoubleClickListener() {
+			
+			@Override
+			public void doubleClick(DoubleClickEvent event) {
+				Viewer viewer = event.getViewer();
+				Shell schell = viewer.getControl().getShell();
+				ISelection sel = viewer.getSelection();
+				
+				Object selectedValue = null;
+				if(!(sel instanceof IStructuredSelection)||sel.isEmpty()){
+					selectedValue = null;
+				}else{
+					selectedValue = ((IStructuredSelection)sel).getFirstElement();
+					if(selectedValue instanceof TimeZone){
+						TimeZone tz = (TimeZone) selectedValue;
+						MessageDialog.openInformation(schell, tz.getID(), tz.toString());
+					}else{
+						MessageDialog.openInformation(schell, "Double click", "Double Click detected");
+						
+					}
+					
+				}
+			}
+		});
+		
+		
+		
 	}
 
 	@Override
