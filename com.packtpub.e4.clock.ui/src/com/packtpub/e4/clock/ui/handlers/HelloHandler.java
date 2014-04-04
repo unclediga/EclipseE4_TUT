79b150d9ba6605104c6b189ec1a744a229cb97b3
BorodinovI <igordin@yandex.ru>
2014-04-04 17:11:57 +0400

    Ch4 : GOOD creating commands and handlers. There was error in MASTER-branch. Try Fix it in this commit.

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/handlers/HelloHandler.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/handlers/HelloHandler.java
@@ -1,21 +1,18 @@
 package com.packtpub.e4.clock.ui.handlers;
 
-import java.util.Map;
-
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.ui.commands.AbstractHandler;
-import org.eclipse.ui.commands.ExecutionException;
 
 public class HelloHandler extends AbstractHandler {
 
 	public HelloHandler() {
 		
 	}
-
 	@Override
-	public Object execute(Map parameterValuesByName) throws ExecutionException {
+	public Object execute(ExecutionEvent event)
+			throws org.eclipse.core.commands.ExecutionException {
 		MessageDialog.openInformation(null, "Hello", "World");
 		return null;
 	}
-
 }
