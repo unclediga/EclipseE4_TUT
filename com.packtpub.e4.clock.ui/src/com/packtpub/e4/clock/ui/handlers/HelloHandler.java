e71ce19403dd0f3a08d86cc4ac4f323e36c623a3
BorodinovI <igordin@yandex.ru>
2014-04-07 12:30:35 +0400

    Jobs and progress : running operations in the background

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/handlers/HelloHandler.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/handlers/HelloHandler.java
@@ -2,7 +2,12 @@ package com.packtpub.e4.clock.ui.handlers;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.widgets.Display;
 
 public class HelloHandler extends AbstractHandler {
 
@@ -12,7 +17,28 @@ public class HelloHandler extends AbstractHandler {
 	@Override
 	public Object execute(ExecutionEvent event)
 			throws org.eclipse.core.commands.ExecutionException {
-		MessageDialog.openInformation(null, "Hello", "World");
+		Job job = new Job("About to say Hello"){
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {};
+				// Exception !!! Invoke UI-function in noUI-thread
+				//MessageDialog.openInformation(null, "Hello", "World");
+				Display.getDefault().asyncExec(new Runnable() {
+					
+					@Override
+					public void run() {
+						MessageDialog.openInformation(null, "Hello", "World");
+						
+					}
+				});
+				return Status.OK_STATUS;
+				
+			}
+			
+		};
+		job.schedule();
 		return null;
 	}
 }
