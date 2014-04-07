57b32dca8169283d375ebb09603e090e3a356bf8
BorodinovI <igordin@yandex.ru>
2014-04-07 16:16:28 +0400

    Ch5: creating a preference page

---
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/ClockPreferencePage.java
@@ -0,0 +1,23 @@
+package com.packtpub.e4.clock.ui;
+
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.jface.preference.IntegerFieldEditor;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+
+public class ClockPreferencePage extends FieldEditorPreferencePage implements
+		IWorkbenchPreferencePage {
+
+	@Override
+	public void init(IWorkbench workbench) {
+		setPreferenceStore(
+				Activator.getDefault().getPreferenceStore());
+	}
+
+	@Override
+	protected void createFieldEditors() {
+		addField(new IntegerFieldEditor("launchCount",
+				"Number of times it has been launched", getFieldEditorParent()));
+	}
+
+}
