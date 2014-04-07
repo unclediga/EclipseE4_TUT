2c9540e69f6d209d7ba6138b309f03a029d56cde
BorodinovI <igordin@yandex.ru>
2014-04-07 16:19:37 +0400

    Ch5: creating warning and error messages

--- com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/ClockPreferencePage.java
+++ com.packtpub.e4.clock.ui/src/com/packtpub/e4/clock/ui/ClockPreferencePage.java
@@ -10,14 +10,17 @@ public class ClockPreferencePage extends FieldEditorPreferencePage implements
 
 	@Override
 	public void init(IWorkbench workbench) {
-		setPreferenceStore(
-				Activator.getDefault().getPreferenceStore());
+		setPreferenceStore(Activator.getDefault().getPreferenceStore());
 	}
 
 	@Override
 	protected void createFieldEditors() {
 		addField(new IntegerFieldEditor("launchCount",
 				"Number of times it has been launched", getFieldEditorParent()));
+		IntegerFieldEditor offset = new IntegerFieldEditor("offset",
+				"Current offset from GMT", getFieldEditorParent());
+		offset.setValidRange(-14, +12);
+		addField(offset);
 	}
 
 }
